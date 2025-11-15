3ac77925293cb1eeaaecaefdf0a79139991ff439
rudra.eshaan2@outlook.com <rudra.eshaan2@outlook.com>
2025-11-15 23:39:40 +0530

    Added Audio Recording and File Saving Functionality

--- app/src/main/java/med/AudioRecorder.java
+++ app/src/main/java/med/AudioRecorder.java
@@ -1,5 +1,47 @@
 package med;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.TargetDataLine;
+
 // Audio recording functionality for the application
 public class AudioRecorder {
-
+    public void  startRecording(File f,BufferedReader sc) {
+        
+            try {
+                AudioFormat af = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44110, 16,2,4,44110,false);
+                DataLine.Info info = new DataLine.Info(TargetDataLine.class,af); 
+                if (!AudioSystem.isLineSupported(info)) System.err.println("unsupported");
+                TargetDataLine targetDataLine = (TargetDataLine)AudioSystem.getLine(info);
+                targetDataLine.open();
+                targetDataLine.start();
+                System.out.println("Audio recording started...");
+                Thread t = new Thread(()->{
+                    AudioInputStream stream = new AudioInputStream(targetDataLine);
+                    try {
+                        AudioSystem.write(stream, AudioFileFormat.Type.WAVE,f);
+                    } catch (IOException e) { System.out.println(e);}
+                    System.out.println("Audio recording stopped.");
+                });
+                t.start();
+                System.out.println("Press enter to stop recording audio.");
+                String s = sc.readLine(); //sc refers to an BufferedReader object
+                
+                targetDataLine.stop();
+                targetDataLine.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            
+           
+        
+    }
 }
--- app/src/main/java/med/main.java
+++ app/src/main/java/med/main.java
@@ -1,8 +1,46 @@
 package med;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 //The main class to run the application and display the output logs and results
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Medical Report Application!");
 
+    /*private static String getDir(String folders,String name) throws IOException{
+        
+        File dir = new File(System.getProperty("user.dir")).getAbsoluteFile();
+        while (dir != null && !dir.getName().equals("app")) {
+            dir = dir.getParentFile();
+            System.out.println(dir);
+        }//walk upward till we reach the 'app' folder if it even exists
+        if (dir == null){
+            dir = new File(System.getProperty("user.dir","/app")); //if 'app' is never found
+        }
+        //creating the path
+        File target = new File(dir,folders);
+        target.mkdirs(); //makes required folders
+        if (!name.isEmpty()) return target.getAbsolutePath();
+        else {
+            target = new File (dir, folders+"/"+name);
+            target.createNewFile();
+            return target.getAbsolutePath();
+        }
+
+    } // to be used at a later time */ 
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Working dir: " + System.getProperty("user.dir"));
+        System.out.println("Java home: " + System.getProperty("java.home"));
+        System.out.println("Java version: " + System.getProperty("java.version"));
+        System.out.println("Welcome to the Medical Report Application!");
+        System.out.print("Please enter to start recording audio: ");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        String s = rd.readLine();
+        AudioRecorder recorder = new AudioRecorder();
+        File f = new File("audios/aud.wav");
+        f.getParentFile().mkdirs();
+        f.createNewFile();
+        recorder.startRecording(f,rd);
+        System.out.println("Method exited");
     }
 }
