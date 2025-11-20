6eae08cb0a9e1cd5d1e1a4f61bf1ddb291efb119
rudra.eshaan2@outlook.com <rudra.eshaan2@outlook.com>
2025-11-20 14:20:20 +0530

    Implement transcribe method in AssemblyAiService for audio transcription

--- app/src/main/java/med/AssemblyAiService.java
+++ app/src/main/java/med/AssemblyAiService.java
@@ -1,5 +1,30 @@
 package med;
 
+import java.io.File;
+import java.net.URI;
+import java.util.HashMap;
+
+import javax.xml.datatype.Duration;
+
 public class AssemblyAiService {
 
+    public String transcribe(String url, String lang) throws Exception{
+        HTTP_Request http = new HTTP_Request(java.time.Duration.ofMinutes(2));
+
+        URI uri = new URI(url);
+
+        HashMap<String,String> data = new HashMap<>();
+        data.put("audio_url", url);
+        data.put("language_code",lang);
+
+        HashMap<String,String> headers = new HashMap<>();
+        headers.put("Authorization", key.assemblyai_apikey.getApikey());
+
+        String[] returnList = {"id"};
+
+        HashMap<String,String> returner = http.POST(uri, data, headers, returnList);
+
+        String id = returner.get("id");
+        return id;
+    }
 }
